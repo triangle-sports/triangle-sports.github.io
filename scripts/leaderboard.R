3e5e40890ac18f65d6be40b1f49a55068711b110
Alexander Fisher <alexander.fisher@duke.edu>
2024-08-08 14:26:29 -0400

    small update task and leaderboard function templates

--- scripts/leaderboard.R
+++ scripts/leaderboard.R
@@ -9,6 +9,19 @@ getScore = function(df) {
   return(score)
 }
 
+# todo: read in game outcome from API
+outcome = data.frame(game_id = 1, 
+                     outcome = 10)
+getMAE = function(df) {
+  mae = 5
+  return(mae)
+}
+
+getCIW = function(df) {
+  ciw = 5
+  return(ciw)
+}
+
 # path to projects
 thePath = "../tsa_projects/2024"
 
@@ -16,16 +29,22 @@ thePath = "../tsa_projects/2024"
 files = list.files(path = thePath)
 
 scores = vector(length = length(files))
+MAEs = vector(length = length(files)) 
+CIWs = vector(length = length(files))
 all_df = list(length = length(files))
 for (i in seq_along(files)) {
   all_df[[i]] = fread(paste0(thePath, "/", files[i]), header = TRUE)
   scores[i] = getScore(all_df[[i]])
+  MAEs[i] = getMAE(all_df[[i]])
+  CIWs = getCIW(all_df[[i]])
 }
 
 teamNames = str_extract(files, "\\w*")
 
 scoreBoard = data.frame(team = teamNames,
-                        score = scores)
+                        score = scores,
+                        MAE = MAEs,
+                        CIW = CIWs)
 
 scoreBoard %>%
   kable(format = "markdown", caption = paste0("Last updated: ", format(Sys.time(), "%a %b %d %Y"))) %>%
