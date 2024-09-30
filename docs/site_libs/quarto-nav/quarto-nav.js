b134eaffe39cf547d8d595c63e8ae64f5badd878
Your name <Email associated with your GitHub account>
2024-09-29 22:28:56 -0400

    add header about changing website

--- docs/site_libs/quarto-nav/quarto-nav.js
+++ docs/site_libs/quarto-nav/quarto-nav.js
@@ -124,9 +124,11 @@ window.document.addEventListener("DOMContentLoaded", function () {
       if (window.Headroom && sidebar.classList.contains("sidebar-unpinned")) {
         sidebar.style.top = "0";
         sidebar.style.maxHeight = "100vh";
+        sidebar.style.minHeight = "100vh";
       } else {
         sidebar.style.top = topOffset + "px";
         sidebar.style.maxHeight = "calc(100vh - " + topOffset + "px)";
+        sidebar.style.minHeight = "calc(100vh - " + topOffset + "px)";
       }
     });
 
@@ -237,7 +239,6 @@ window.document.addEventListener("DOMContentLoaded", function () {
     const links = window.document.querySelectorAll("a");
     for (let i = 0; i < links.length; i++) {
       if (links[i].href) {
-        links[i].dataset.originalHref = links[i].href;
         links[i].href = links[i].href.replace(/\/index\.html/, "/");
       }
     }
